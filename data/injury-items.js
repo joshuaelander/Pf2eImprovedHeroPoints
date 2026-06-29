5ba856065e5cf62b843d7731972bd1864997c062
joshuaelander <joshuaelander@gmail.com>
2026-06-30 00:40:58 +0900

    Add preset rules

--- data/injury-items.js
+++ data/injury-items.js
@@ -9,6 +9,70 @@ const PRESET_RULES = {
                 { mode: "override", property: "system.persistent.formula", value: "1d4" }
             ]
         }
+    ],
+    "Pulled Hamstring": [
+        { key: "FlatModifier", selector: "speed", value: -5, type: "status" }
+    ],
+    "Tunnel Vision": [
+        { key: "FlatModifier", selector: "perception", value: -2, type: "status" }
+    ],
+    "Twisted Ankle": [
+        { key: "FlatModifier", selector: "land-speed", value: -5, type: "status" }
+    ],
+    "Battered": [
+        { key: "Weakness", type: "physical", value: 2 }
+    ],
+    "Muscle Spasm": [
+        { key: "FlatModifier", selector: "str-based", value: -2, type: "status" },
+        { key: "FlatModifier", selector: "dex-based", value: -2, type: "status" }
+    ],
+    "Migraine": [
+        { key: "FlatModifier", selector: "int-based", value: -2, type: "status" },
+        { key: "FlatModifier", selector: "wis-based", value: -2, type: "status" }
+    ],
+    "Social Faux Pas": [
+        { key: "FlatModifier", selector: "cha-based", value: -2, type: "status" }
+    ],
+    "Arcane Static": [
+        { key: "FlatModifier", selector: "perception", value: -2, type: "status" }
+    ],
+    "Grounded Energy": [
+        { key: "FlatModifier", selector: "speed", value: -5, type: "status" }
+    ],
+    "Ringing Ears": [
+        { key: "FlatModifier", selector: "perception", value: -2, type: "status" }
+    ],
+    "Stiff Leg": [
+        { key: "FlatModifier", selector: "land-speed", value: -5, type: "status" }
+    ],
+    "Minor Fatigue": [
+        { key: "FlatModifier", selector: "skill-check", value: -1, type: "status" }
+    ],
+    "Sore Muscles": [
+        { key: "FlatModifier", selector: "athletics", value: -2, type: "status" },
+        { key: "FlatModifier", selector: "acrobatics", value: -2, type: "status" }
+    ],
+    "Dazed": [
+        { key: "FlatModifier", selector: "initiative", value: -2, type: "status" }
+    ],
+    "Aches and Pains": [
+        { key: "Weakness", type: "physical", value: 1 }
+    ],
+    "Short of Breath": [
+        { key: "FlatModifier", selector: "fortitude", value: -1, type: "status" }
+    ],
+    "Slow Reflexes": [
+        { key: "FlatModifier", selector: "reflex", value: -1, type: "status" }
+    ],
+    "Shaken": [
+        { key: "FlatModifier", selector: "will", value: -1, type: "status" }
+    ],
+    "Labored Breathing": [
+        { key: "FlatModifier", selector: "stealth", value: -2, type: "status" }
+    ],
+    "Feverish": [
+        { key: "FlatModifier", selector: "saving-throw", value: -1, type: "status", predicate: ["poison"] },
+        { key: "FlatModifier", selector: "saving-throw", value: -1, type: "status", predicate: ["disease"] }
     ]
 };
 
